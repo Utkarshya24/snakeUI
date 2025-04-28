0b5d87a181251bd992a702c9d642b5f19dac934d
Utkarsh Yadav <utky24@gmail.com>
2025-04-28 17:12:01 +0000

    setup button layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
-import Button from "utkarshya24-ui/src/components/button"
+import Button from '@/src/components/button'
 
 export default function Home() {
+
+  
   return (
     <div className="p-4">
-      <h1>Test UI Library</h1>
-      <Button >
+      <p className='text-[18px]'>Test UI Library</p>
+      <Button varient='error'>
         Click Me
       </Button>
     </div>
--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,17 +1,58 @@
-import React from 'react'
+import React from 'react';
 
-interface types{
-    className ? : string;
-    children ? : React.ReactNode; 
+interface ButtonProps {
+  varient?: "default" | "error"
+  className?: string;
+  children?: React.ReactNode; 
+  style?: React.CSSProperties; 
+  onClick?: (event: React.MouseEvent<HTMLButtonElement>) => void; 
+  disabled?: boolean;
 }
 
+const Button: React.FC<ButtonProps> = ({
+  varient = "default",
+  className = '',
+  children,
+  style,
+  onClick,
+  disabled = false,
+}) => {
+ 
+  const baseStyles = `
+    flex items-center justify-center 
+    px-4 py-2
+    transition-colors duration-200 
+    focus:outline-none focus:ring-2 focus:ring-offset-2
+    ${disabled ? 'cursor-not-allowed opacity-50' : 'cursor-pointer'}
+  `;
 
-const Button : React.FC<types> = ({className, children }: types) => {
+  const varientStyle = {
+    default:`
+      text-color
+      bg-accent
+      hover:bg-primary
+      focus:ring-primary
+      ${disabled ? 'bg-gray-400' : ''}
+    `,
+    error:`
+      text-white
+      bg-red-600
+      hover:bg-red-800
+      focus:ring-red-800
+      ${disabled ? 'bg-gray-400' : ''}
+    `
+  }
+
   return (
-    <div>
-        <button className={`${className}`}>{children}</button>
-    </div>
-  )
-}
+    <button
+      className={`${baseStyles} ${varientStyle[varient]} ${className}`}
+      style={style}
+      onClick={onClick}
+      disabled={disabled}
+    >
+      {children}
+    </button>
+  );
+};
 
-export default Button
+export default Button;
