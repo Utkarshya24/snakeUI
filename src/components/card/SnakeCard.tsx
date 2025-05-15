bb637a6c621e50aa8e572d052f00116886c21f4a
utkarshya24 <utky24@gmail.com>
2025-05-15 05:34:36 +0000

    Add SnakeCard and SnakeCardCarousel components with variants and carousel functionality; update Navbar for card navigation

---
+++ src/components/card/SnakeCard.tsx
@@ -0,0 +1,21 @@
+export {
+  SnakeCard,
+  SnakeCardHeader,
+  SnakeCardContent,
+  SnakeCardFooter,
+  SnakeCardPagination,
+} from './card';
+export { SnakeCardCarousel } from './SnakeCardCarousel';
+
+export { cardVariants } from './variants';
+export { useCarousel } from './useCarousel';
+
+export type {
+  SnakeCardProps,
+  SnakeCardHeaderProps,
+  SnakeCardContentProps,
+  SnakeCardFooterProps,
+  SnakeCardPaginationProps,
+  BaseCardProps,
+  SnakeCardCarouselProps,
+} from './types';
